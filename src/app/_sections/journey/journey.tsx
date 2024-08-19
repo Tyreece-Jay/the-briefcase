085875cbbad2407b89f63ddc0968f76d938cee28
Tyreece-Jay <tyreece.jay@gmail.com>
2024-08-19 18:10:01 +0100

    #2

--- src/app/_sections/journey/journey.tsx
+++ src/app/_sections/journey/journey.tsx
@@ -7,7 +7,6 @@ import { content, JourneyType } from '@/app/content'
 import { match } from 'ts-pattern'
 import { Text } from "@/app/_design/text/text"
 import { AnimatePresence, motion, LayoutGroup } from 'framer-motion'
-import Image from 'next/image'
 
 export default function Journey() {
   const [selectedJourney, setSelectedJourney] = useState<JourneyType | undefined>()
@@ -49,7 +48,7 @@ function Modal({ selectedJourney, setSelected }: { selectedJourney: JourneyType,
       <motion.div layoutId={selectedJourney} className={modal}>
         <Heading importance={5} colour={'black'}>{`// ${journeyContent.type}`}</Heading>
         <div className={modalContent}>
-          <Image src={`${process.env.NODE_ENV == "production" ? "/the-briefcase" : undefined}${journeyContent.image.src}`} alt={journeyContent.alt} />
+          <img src={journeyContent.image.src} alt={journeyContent.alt} />
           <span>{journeyContent.information}</span>
         </div>
       </motion.div>
@@ -64,7 +63,7 @@ function JourneyCard({ type, selected, setSelected }: JourneyCardType) {
 
   return <motion.div layoutId={type} className={card} onClick={() => { selected == type ? setSelected(undefined) : setSelected(type) }}>
     <Heading importance={5} colour={'black'}>{`// ${journeyContent.type}`}</Heading>
-    <img src={`${process.env.NODE_ENV == "production" ? "/the-briefcase" : undefined}${journeyContent.image.src}`} alt={journeyContent.alt} />
+    <img src={journeyContent.image.src} alt={journeyContent.alt} />
     <div className={footer}>
       <Text>Click Me</Text>
       <span className={dots}>
