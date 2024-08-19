c572645f73e9414b817fbb4712613747e343ced8
Tyreece-Jay <tyreece.jay@gmail.com>
2024-08-19 16:57:36 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,13 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 
 const withVanillaExtract = createVanillaExtractPlugin()
 
+let path = ''
+if(process.env.NODE_ENV == "production"){
+  path = "/the-briefcase"
+}
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/the-briefcase",
+  basePath: path,
   // output: "export",
   // reactStrictMode: true
 }
