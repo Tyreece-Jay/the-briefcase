ea5249b4468f829e9cd674672d1796829332f069
Tyreece-Jay <tyreece.jay@gmail.com>
2024-09-06 08:44:09 +0100

    removed drag

--- src/app/_sections/about/about.tsx
+++ src/app/_sections/about/about.tsx
@@ -9,8 +9,8 @@ import { background, container, left, right, section } from "./about.css"
 export default function About() {
 
   return <section id="about" className={section}>
-    <div className={background}/>
-    <motion.div className={container} drag dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }} >
+    <div className={background} />
+    <div className={container}>
       <div className={left}>
         <Heading importance={3}>ABOUT ME</Heading>
         <Button href="mailto:tyreece.jay@gmail.com">Send me an email</Button>
@@ -19,6 +19,6 @@ export default function About() {
         <Text>{content.about}</Text>
         <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
       </div>
-    </motion.div>
+    </div>
   </section>
 }
