d6d5f4270075028791cb47ad712feb00bd7bbb96
Tyreece-Jay <tyreece.jay@gmail.com>
2024-11-12 18:54:52 +0000

    -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { content } from './content'
 import { themeClass } from './_design/theme.css'
 import { Montserrat } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-inter', display: 'swap' })
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang='en' className={`${themeClass} ${montserrat.className}`}>
       <body>
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
