ec6abb2735c6c886d79517337bab2ffec7c9c77a
Tyreece-Jay <tyreece.jay@gmail.com>
2024-11-12 18:53:32 +0000

    -

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -4,7 +4,7 @@ interface EmailTemplateProps { name: string, message: string, email: string }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({name, message, email}) => (
   <div>
-    <h1>New messae from {name}!</h1>
+    <h1>New message from {name}!</h1>
     <h3>reply using {email}</h3>
     <p>{message}</p>
   </div>
