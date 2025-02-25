f491b0b8992c63c8b3cf00580f6f5edbcd8d3213
Tyreece-Jay <tyreece.jay@gmail.com>
2025-02-25 23:29:50 +0000

    Update parallax-text.tsx

--- src/app/_design/parallax-text.tsx/parallax-text.tsx
+++ src/app/_design/parallax-text.tsx/parallax-text.tsx
@@ -11,14 +11,14 @@ export function ParallaxText({ direction, children }: { direction: 'Forward' | '
   const { scrollY } = useScroll()
   const scrollVelocity = useVelocity(scrollY)
   const smoothVelocity = useSpring(scrollVelocity, { damping: 50, stiffness: 400 })
-  const velocityFactor = useTransform(smoothVelocity, [0, 100000], [0, 5], { clamp: false })
+  const velocityFactor = useTransform(smoothVelocity, [0, 1], [0, 5], { clamp: false })
 
   const x = useTransform(baseX, (v: number) => `${wrap(-20, -45, v)}%`)
 
   const directionFactor = useRef<number>(1)
   useAnimationFrame(() => {
     const velocity = direction === 'Forward' ? 1 : -1
-    const moveBy = directionFactor.current * velocity / 300
+    const moveBy = directionFactor.current * velocity / 400
 
     if (velocityFactor.get() < 0) {
       directionFactor.current = -1
